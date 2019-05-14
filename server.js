ad2b23815d68e0796588db1708d783ed38ee1920
Justin D. Cotton <justin.d.cotton.esq@gmail.com>
2019-05-14 11:33:16 -0400

    Updating client side

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get('/', (req,res) => {
-  res.send('Hello world!')
-})
+app.use(express.static(`${__dirname}/client/build`))
+
+// app.get('/', (req,res) => {
+//   res.send('Hello world!')
+// })
+
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+  })
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
