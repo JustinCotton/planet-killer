8b63b70ea228405149f48a40634c302cdc0ec0cb
Justin D. Cotton <justin.d.cotton.esq@gmail.com>
2019-05-16 16:27:18 -0400

    removes duplicate handler to /

--- server.js
+++ server.js
@@ -6,15 +6,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(`${__dirname}/client/build`))
 
+/*
 app.get('/', (req,res) => {
   res.send('Ready to wreak havoc!')
 })
-
-app.use('/', routes)
+*/
 
 app.get('/*', (req, res) => {
     res.sendFile(`${__dirname}/client/build/index.html`)
 })
+
+app.use('/', routes)
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
