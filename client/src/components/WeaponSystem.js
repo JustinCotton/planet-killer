58d155fd3dfd3689af229bd4e5d6ef8349809f1e
Justin D. Cotton <justin.d.cotton.esq@gmail.com>
2019-05-24 09:18:09 -0400

    Fully functional full-stack MERN app, deployed to Heroku

--- client/src/components/WeaponSystem.js
+++ client/src/components/WeaponSystem.js
@@ -5,12 +5,20 @@ class WeaponSystem extends Component {
 
   render() {
     const imgStyle = {
-        width: "80%",
+        width: "70%",
         margin: "20px",
         borderRadius: "300px"
       }
+      const missionStyle = {
+        fontSize: "30px",
+        fontWeight: "bold",
+        margin: "0px"
+      }
     return (
-        <div><img src={weaponSystem} style={imgStyle}/></div>
+        <div>
+          <img src={weaponSystem} style={imgStyle}/>
+          <p style={missionStyle}>Create, Transform, and Destroy Planets and Celestial Bodies!</p>
+        </div>
     )
   }
 }
